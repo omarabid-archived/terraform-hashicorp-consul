e70be57102db06b5ecf374f3311981d9f86cec46
Abid Omar <contact@omarabid.com>
2020-01-02 17:37:02 +0100

    last version

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "server_config" {
 	value = "${data.template_file.server_config.*.rendered}"
 }
 
+output "acl_config" {
+	value = "${data.template_file.acl_config.*.rendered}"
+}
+
 output "client_config" {
 	value = "${data.template_file.client_config.*.rendered}"
 }
--- variables.tf
+++ variables.tf
@@ -3,6 +3,7 @@ variable "server_domain" {}
 variable "server_hostnames" {
 	type = "map"
 }
+variable "acl_token" {}
 
 # Server Config File
 data "template_file" "server_config" {
@@ -12,6 +13,19 @@ data "template_file" "server_config" {
 	vars {
 		node_name = "consul-${var.server_domain}-${count.index + 1}"
 		server_domain = "${var.server_domain}"
+		acl_token = "${var.acl_token}"
+	}
+}
+
+# ACL Config File
+data "template_file" "acl_config" {
+	count = "${length(var.server_hostnames)}"
+	template = "${file("${path.module}/server/acl.json")}"
+
+	vars {
+		node_name = "consul-${var.server_domain}-${count.index + 1}"
+		server_domain = "${var.server_domain}"
+		acl_token = "${var.acl_token}"
 	}
 }
 
@@ -22,5 +36,6 @@ data "template_file" "client_config" {
 	vars {
 		node_name = "consul-central-client"
 		server_domain = "${var.server_domain}"
+		acl_token = "${var.acl_token}"
 	}
 }
